04440ca6c723254199d551f312dd358a0a3ec77b
Aleksei Dubrovin <53064004+alekcena@users.noreply.github.com>
2025-09-15 13:19:10 +0500

    first commit

---
+++ _next/static/chunks/f8f16572b3c70f19.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/b0235afc7245ff01.js",
+  "static/chunks/turbopack-e9964554634430b7.js"
+])
---
+++ _next/static/we6fqxdlFotH-AnDFcLiH/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/4085017209823deb.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
